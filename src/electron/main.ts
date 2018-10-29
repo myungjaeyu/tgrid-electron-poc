d6f6c4f5fcc87d6a52db38409d58327af324651b
myungjaeyu <rootcode10@gmail.com>
2018-10-29 15:13:34 +0900

    Added electron app.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,4 +1,11 @@
+import MainWindowService from './internal/MainWindowService'
+
 async function main() : Promise<void> {
+
+    const mainWindowService = new MainWindowService()
+
+    mainWindowService.setHtmlPath('index.html')
+    mainWindowService.run()
 
 }
 
