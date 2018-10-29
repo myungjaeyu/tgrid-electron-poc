33e4992e8f71d54da12f680dde12ee924f0b6cf6
myungjaeyu <rootcode10@gmail.com>
2018-10-29 20:41:58 +0900

    Added web connector.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,11 +1,31 @@
 import MainWindowService from './internal/MainWindowService'
 
+
+import { WebConnector } from 'tgrid/protocol/web'
+import { Driver } from 'tgrid/base'
+
+import IExam from '../tgrid-exam/IExam'
+
 async function main() : Promise<void> {
 
     const mainWindowService = new MainWindowService()
 
     mainWindowService.setHtmlPath('index.html')
-    mainWindowService.run()
+    mainWindowService.run() // console.log('Symbol.iterator', Symbol.iterator)
+
+
+    /* Hack
+    */
+    const connector = new WebConnector()
+
+    await connector.connect('ws://127.0.0.1:10101')
+    await connector.wait()
+
+    const exam : Driver<IExam> = connector.getDriver<IExam>()
+
+    console.log(await exam.send('u4bi'))
+
+    await connector.close()
 
 }
 
